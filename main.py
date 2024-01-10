f8fdf2b22fdbf0599d9f4f042b0a4331ec8a26dc
muxammad <ibrohimovmuhammad2020@gmail.com>
2024-01-10 18:19:11 +0100

    adding authenticate method by using inheritance

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 df = pd.read_csv("hotels.csv", dtype={"id": str})
 df_Card = pd.read_csv("cards.csv", dtype=str)
+df_Secure = pd.read_csv("card_security.csv", dtype=str)
 
 
 class Hotel:
@@ -41,33 +42,48 @@ class ReservationTicket:
         """
         return content
 
-
 class CreditCard:
     def __init__(self, number):
         self.number = number
 
-    def validate(self, expiration, cvs, holder):
+    def validate(self, expiration, cvc, holder):
         card_data = {"number": self.number, "expiration": expiration,
-                   "csv": cvs, "holder": holder}
-        if card_data in df_Card:
+                     "cvc": cvc, "holder": holder}
+
+        df_list = df_Card.to_dict('records')
+
+        if card_data in df_list:
             return True
         else:
             return False
 
+
+class SecureCard(CreditCard):
+    def authenticate(self, given_password):
+        password = df_Secure.loc[df_Secure["number"] == self.number, "password"].squeeze()
+        if password == given_password:
+            return True
+        else:
+            return False
 
 
 print(df)
 hotel_id = input("Enter a hotel id number: ")
 hotel = Hotel(hotel_id)
 
+""" In order to make a simple received all the credit card data from csv not from database,
+ normally all the data must be taken from user but here I gave it on my own"""
 
 if hotel.available():
-    creditcard = CreditCard()
-    if creditcard.validate():
-        hotel.book()
-        name = input("Enter your name: ")
-        reservation_ticket = ReservationTicket(customer_name=name, hotel_objects=hotel)
-        print(reservation_ticket.generate())
+    creditcard = SecureCard(number="1234567890123456")
+    if creditcard.validate(expiration="12/26", cvc="123", holder="JOHN SMITH"):
+        if creditcard.authenticate(given_password="mypass"):
+            hotel.book()
+            name = input("Enter your name: ")
+            reservation_ticket = ReservationTicket(customer_name=name, hotel_objects=hotel)
+            print(reservation_ticket.generate())
+        else:
+            print("Authenticate was failed")
     else:
         print("Payment was unsuccessfull: ")
 else:
